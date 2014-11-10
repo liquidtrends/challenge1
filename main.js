baa02d30b34b961a8f13e873448156a359066572
Ryan Janvier <ryan@liquidtrends.com>
2014-11-10 13:35:04 -0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+
+var data = { countries: [
+    {code: "BY", CountryName: "Belarus", Population: 9685000, English: false},
+    {code: "BZ", CountryName: "Belize", Population: 314522, English: true},
+    {code: "CA", CountryName: "Canada", Population: 33679000, English: true},
+    {code: "CC", CountryName: "Cocos [keeling] Islands", Population: 628, English: true},
+    {code: "CD", CountryName: "Democratic Republic of the Congo", Population: 70916439, English: false},
+    {code: "CF", CountryName: "Central African Republic", Population: 4844927, English: false},
+    {code: "CG", CountryName: "Republic of the Congo", Population: 3039126, English: false},
+    {code: "CH", CountryName: "Switzerland", Population: 7581000, English: false},
+  ]};
+$('body').append(template(data));
+
+
+Handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
+    if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+    if( lvalue!=rvalue ) {
+        return options.inverse(this);
+    } else {
+        return options.fn(this);
+    }
+});
